0bd8ae9931bc792e649b1b6c7496816e45c01b63
arfath19 <37550475+arfath19@users.noreply.github.com>
2020-03-12 17:10:50 +0530

    Update calender.js
    Final Commitment with previous and new darkened days

--- calender.js
+++ calender.js
@@ -1,4 +1,3 @@
-
 let monthCount=new Date().getMonth()+1;
 
 const monthsName=['January','February','March','April','May','June','July','August','September','October','November','December'];
@@ -23,17 +22,27 @@ function next(){
  
 
 display(new Date(2020,monthCount,0).getDate());
-function display(date){
+function display(date){ 
     const displayMonth=document.querySelector('.month');
     const currMonth=new Date().getMonth()+1;
     let currDay = (new Date(2020,monthCount-1,01).getDay());
-
+    let lastDay = (6-(new Date(2020,monthCount-1,date).getDay())); //to get end light dates
+    let FirstDay = ((new Date(2020,monthCount-1,01).getDay()));//to get the first light dates count
+    let dateNew=new Date(2020,monthCount-1,0).getDate();
+    let firstDayCalc=dateNew; // to get the first day 
+    let minus=FirstDay; // to get minus val for firstDayCAlc add 1 to get previous month
+    // console.log("curr month date"+dateNew+" "+ firstDayCalc);
     displayMonth.innerHTML=monthsName[monthCount-1];
     const day=[];
-    for(i=currDay;i>0;i--){
-            day.push(`<li></li>`);        
+    // for(i=1;i<=FirstDay;i++){ //dont touch
+    //     day.push(`<li class="light ${i}">${i}</li>`);
+    // }
+
+    for(i=(firstDayCalc-minus)+1;FirstDay>0;i++){
+            day.push(`<li class="light ${i}">${i}</li>`);  
+            FirstDay--; 
     }
-    console.log(day);
+    // console.log(day);
     for(i=1;i<=date;i++){
     if(i==currDate && currMonth==monthCount){
         day.push(`<li class="${i} active" > ${i}</li>`)
@@ -41,7 +50,12 @@ function display(date){
         day.push(`<li class="${i}">${i}</li>`);
 
     }
-}
+    
+    }
+    for(i=1;i<=lastDay;i++){
+        day.push(`<li class="light ${i}">${i}</li>`);
+    }
+
     calender.innerHTML=day.join("");
 }
 
